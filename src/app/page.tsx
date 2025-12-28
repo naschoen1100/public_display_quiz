99fd6e2ec4fee6ae7b4b5a40199464021f029f7d
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2025-12-28 20:36:55 +0100

    deleted db upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {getQuizNames} from "@/app/data/getQuizData";
 import {importQuizzes} from "@/app/data/createQuizData";
 export const dynamic = "force-dynamic";
 export default async function EntryPage () {
-    await importQuizzes()
+    //await importQuizzes()
     const dataPromise = getQuizNames();
     return (
         <>
