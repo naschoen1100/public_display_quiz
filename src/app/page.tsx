dd0015e9b3755f30d8bc966546923ed0a45fe66e
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2026-01-04 15:27:55 +0100

    changed language on entry page to english

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function EntryPage () {
         <>
             <div className={"flex flex-col items-center justify-center bg-cyan-600"}>
                 <h1 className="text-6xl md:text-8xl lg:text-9xl font-extrabold text-center uppercase p-4 text-cyan-900">
-                    Willkommen!
+                    Welcome!
                 </h1>
                 <div className="mt-2">
                     <Suspense fallback={<p className={"text-center mt-6"}> ...Loading </p>}>
