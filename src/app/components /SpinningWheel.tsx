3c24543429d80d15533c4870d8a09429e7093dcc
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2026-01-11 14:16:38 +0100

    updated dependecies, changed spinning wheel adapted viewport export

--- src/app/components /SpinningWheel.tsx
+++ src/app/components /SpinningWheel.tsx
@@ -1,11 +1,11 @@
 "use client";
 import {use, useEffect, useRef, useState} from "react";
-import { Wheel } from "react-custom-roulette";
 import {useRouter} from "next/navigation";
 import {getQuizNames} from "@/app/data/getQuizData";
 import {createUserWithScore} from "@/app/data/handleUser";
-import {B612} from "next/dist/compiled/@next/font/dist/google";
 import Button from "@/app/components /Button";
+import { SpinWheel } from "react-spin-wheel"
+import "react-spin-wheel/dist/index.css"
 
 type SpinningWheelProps = {
     dataPromise: Promise<Awaited<ReturnType<typeof getQuizNames>>>;
@@ -18,26 +18,11 @@ export default function SpinningWheel({dataPromise}: SpinningWheelProps) {
     const [button1Pressed, setButton1Pressed] = useState(false);
     const [button2Pressed, setButton2Pressed] = useState(false);
     const [prizeNumber, setPrizeNumber] = useState(0);
-    const containerRef = useRef(null);
-    const [size, setSize] = useState(600);
     const handleSpin = () => {
         const randomIndex = Math.floor(Math.random() * data.length);
         setPrizeNumber(randomIndex);
         setMustSpin(true);
     };
-
-    useEffect(() => {
-        const observer = new ResizeObserver(entries => {
-            const { width, height } = entries[0].contentRect;
-            setSize(Math.min(width, height));
-        });
-
-        if (containerRef.current) {
-            observer.observe(containerRef.current);
-        }
-
-        return () => observer.disconnect();
-    }, []);
 
 
     const handleStop = async () => {
@@ -64,31 +49,18 @@ export default function SpinningWheel({dataPromise}: SpinningWheelProps) {
         }
     }
 
-    const labels = data.map(q => ({
-        option: q.title,
-    }));
+    const labels: string[] = data.map(q => q.title);
 
     return (
     <div className="flex flex-col items-center justify-center p-10">
         <div className= "w-full max-w-[85vmin] aspect-square flex items-center justify-center">
-            <Wheel
-                mustStartSpinning={mustSpin}
-                prizeNumber={prizeNumber}
-                data={labels}
-                onStopSpinning={handleStop}
-                backgroundColors={["#9ae600", "#5ea500"]}
-
-                textColors={["#000"]}
-                outerBorderColor={"#000"}
+            <SpinWheel
+                items={labels}
+                itemColors={["#9ae600", "#5ea500"]}
+                borderColor="#000"
+                spinTime={3000}
+                onFinishSpin={handleStop}
             />
-        </div>
-        <div className="flex items-center justify-center">
-            <Button onClick={handleOnClickButton1} variant={"primary"} disabled={button1Pressed}>
-                Lets Play
-            </Button>
-            <Button onClick={handleOnClickButton2} variant={"primary"} disabled={button2Pressed}>
-                Lets Play
-            </Button>
         </div>
     </div>
 );
