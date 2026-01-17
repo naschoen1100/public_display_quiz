027e0e83ea7ccc95aa945141912b1457fd9e64e5
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2026-01-17 11:25:06 +0100

    core: fix trailing whitespace in name again

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,15 +1,15 @@
-import { Suspense } from "react";
-import { getQuizQestions } from "@/app/data/getQuizData";
-import QuizPage from "@/app/components /Quiz";
-export const dynamic = "force-dynamic";
+import { Suspense } from 'react';
+import { getQuizQestions } from '@/app/data/getQuizData';
+import QuizPage from '@/app/components/Quiz';
+export const dynamic = 'force-dynamic';
 export default function Page() {
-    const dataPromise = getQuizQestions();
+  const dataPromise = getQuizQestions();
 
-    return (
-        <>
-            <Suspense fallback={<p className={"text-center mt-6"}> ...Loading </p>}>
-                <QuizPage dataPromise = {dataPromise}/>
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <Suspense fallback={<p className={'text-center mt-6'}> ...Loading </p>}>
+        <QuizPage dataPromise={dataPromise} />
+      </Suspense>
+    </>
+  );
 }
--- src/app/quiz/result/page.tsx
+++ src/app/quiz/result/page.tsx
@@ -1,16 +1,16 @@
-import { Suspense } from "react";
-import ResultPage from "@/app/components /ResultPage";
-import { getUserScore } from "@/app/data/handleAnswerStatistics";
+import { Suspense } from 'react';
+import ResultPage from '@/app/components/ResultPage';
+import { getUserScore } from '@/app/data/handleAnswerStatistics';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-    const dataPromise = getUserScore()
-    return (
-        <>
-            <Suspense fallback={<p className={"text-center mt-6"}> ...Loading </p>}>
-                <ResultPage dataPromise = {dataPromise}/>
-            </Suspense>
-        </>
-    );
+  const dataPromise = getUserScore();
+  return (
+    <>
+      <Suspense fallback={<p className={'text-center mt-6'}> ...Loading </p>}>
+        <ResultPage dataPromise={dataPromise} />
+      </Suspense>
+    </>
+  );
 }
