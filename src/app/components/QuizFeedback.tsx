7f880ccf9a247255ad8655b5f91bca37f85ad1f4
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2026-01-17 11:26:03 +0100

    core: fix trailing whitespace in name again

---
+++ src/app/components/QuizFeedback.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { UIQuestion } from '@/app/types/types';
+import Feedback from '@/app/components/Feedback';
+import { useState } from 'react';
+import RankingModal from '@/app/components/RankingModal';
+import Score from '@/app/components/Score';
+
+type FeedbackProps = {
+  question: UIQuestion;
+  selectedIndex: number;
+  onNext: () => void;
+};
+
+export default function QuizFeedback({ question, selectedIndex, onNext }: FeedbackProps) {
+  const isCorrect = selectedIndex === question.correctAnswer;
+  const [open, setOpen] = useState(false);
+  const handleNext = async (): Promise<void> => {
+    onNext();
+  };
+  return (
+    <>
+      <div className="card w-full max-w-[80vmin] aspect-[4/4] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-3xl p-[clamp(1rem,2vmin,3rem)] flex flex-col justify-between overflow-hidden">
+        <div
+          className="absolute top-[clamp(0.8rem,2vmin,2rem)] right-[clamp(0.8rem,0.5vmin,0.7rem)] bg-gray-200 text-slate-800 font-bold
+                                px-[clamp(0.8rem,2.5vmin,4.5rem)] py-[clamp(0.4rem,2.3vmin,4rem)] rounded-full text-[clamp(1rem,2.3vmin,4rem)] shadow-lg"
+        >
+          <Score />
+        </div>
+        {/* Header: Feedback */}
+        <div
+          className={`text-center font-bold py-[clamp(0.8rem,4vmin,5rem)] text-[clamp(1.5rem,3vmin,4rem)] -mx-[clamp(1rem,2vmin,3rem)] -mt-[clamp(1rem,2vmin,3rem)] mb-[clamp(1rem,2vmin,3rem)]"
+                    ${isCorrect ? 'bg-green-100 text-green-700' : 'bg-red-200 text-red-700'} shadow-md`}
+        >
+          {isCorrect ? 'Right Answer' : 'Wrong Answer'}
+        </div>
+
+        {/* Body: Question + Answers + Feedback */}
+        <div className="flex flex-1 gap-[clamp(1rem,2.5vmin,3rem)] mt-[clamp(1rem,2vmin,2rem)] overflow-y-auto">
+          {/* Question + Answers */}
+          <div className="flex-1 flex flex-col gap-[clamp(0.5rem,1vmin,1.5rem)]">
+            {/* Question */}
+            <div className="text-center text-[clamp(1.5rem,3vmin,4rem)] font-semibold text-white p-[clamp(0.5rem,1vmin,1rem)]">
+              {question.text}
+            </div>
+
+            {/* Answers */}
+            <div className="flex flex-col gap-[clamp(0.5rem,1vmin,1rem)]">
+              {question.answers.map((answer, index) => {
+                const isCorrectAnswer = index === question.correctAnswer;
+                const isSelected = index === selectedIndex;
+                return (
+                  <div
+                    key={index}
+                    className={`px-[clamp(0.5rem,1vmin,1rem)] py-[clamp(0.5rem,1vmin,1.5rem)] rounded-xl text-[clamp(1.2rem,2vmin,3rem)] font-bold
+                                                  transition-all duration-200 transform hover:scale-105
+                                                  ${isCorrectAnswer ? 'border-green-400 bg-green-600 text-white' : ''}
+                                                  ${isSelected && !isCorrectAnswer ? 'border-red-400 bg-red-500 text-white' : ''}
+                                                  ${!isCorrectAnswer && !isSelected ? 'border-slate-300 bg-slate-600 text-white' : ''}`}
+                  >
+                    {answer}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="mt-[clamp(0.5rem,1.5vmin,3rem)] p-[clamp(0.5rem,1vmin,1rem)] text-[clamp(1rem,3.5vmin,4.5rem)] text-white-500 text-center">
+              <Feedback questionId={question.id} />
+            </div>
+          </div>
+        </div>
+
+        {/* Footer */}
+        <div className="flex flex-col gap-[clamp(0.5rem,1vmin,1.5rem)] mt-[clamp(1rem,2vmin,2rem)]">
+          <button
+            className="btn w-full py-[clamp(0.5rem,2.5vmin,3rem)] text-[clamp(1rem,2vmin,3rem)] bg-gradient-to-r from-cyan-500 to-sky-500 hover:from-sky-500 hover:to-cyan-500 text-white rounded-xl shadow-lg transition-all"
+            onClick={() => setOpen(true)}
+          >
+            Next Question
+          </button>
+        </div>
+      </div>
+      <RankingModal open={open} onClose={() => setOpen(false)} onAfterClose={handleNext} />
+    </>
+  );
+}
---
+++ src/app/components/ResultPage.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { use } from 'react';
+import { useRouter } from 'next/navigation';
+import { getUserScore } from '@/app/data/handleAnswerStatistics';
+import { useInactivityTimeout } from '@/app/util/useInactivityTimeout';
+import Rank from '@/app/components/rank';
+
+type ResultPageProps = {
+  dataPromise: Promise<Awaited<ReturnType<typeof getUserScore>>>;
+};
+export default function ResultPage({ dataPromise }: ResultPageProps) {
+  useInactivityTimeout(60000 * 3);
+  const data = use(dataPromise);
+  const router = useRouter();
+  const points = data?.score?.points ?? 0;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-600 to-slate-800 p-4">
+      <div className="card w-full max-w-[70vmin] aspect-[4/3] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-2xl p-[clamp(1rem,1.5vmin,2rem)] flex flex-col justify-center items-center gap-[clamp(1rem,2vmin,2rem)]">
+        {/* Header */}
+        <h1 className="text-[clamp(2rem,4vmin,6rem)] font-extrabold text-center uppercase">
+          Quiz Finished!
+        </h1>
+
+        {/* Score */}
+        <p className="text-[clamp(1.5rem,3vmin,4rem)] font-semibold text-center text-white">
+          Your score: {points} / 3
+        </p>
+
+        {/* Ranking / Leaderboard */}
+        <div className="w-full text-[clamp(1.5rem,3vmin,4rem)]  flex justify-center mt-[clamp(0.5rem,1.5vmin,2rem)]">
+          <Rank />
+        </div>
+
+        {/* Button */}
+        <button
+          className="btn py-[clamp(0.6rem,1.5vmin,2.5rem)] text-[clamp(1rem,1.8vmin,4rem)] bg-gradient-to-r from-cyan-500 to-sky-500 hover:from-sky-500 hover:to-cyan-500 text-white rounded-xl shadow-lg transition-all mt-[clamp(0.5rem,1vmin,2rem)]"
+          onClick={() => router.push(`/`)}
+        >
+          Start Again
+        </button>
+      </div>
+    </div>
+  );
+}
