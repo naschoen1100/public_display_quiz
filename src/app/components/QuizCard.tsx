77410e9b1932f1f2077d89e93778deef3c10e386
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2026-01-18 11:42:33 +0100

    core: removed scolling and adjusted headline size

--- src/app/components/QuizCard.tsx
+++ src/app/components/QuizCard.tsx
@@ -1,99 +1,96 @@
-'use client'
-import React, { useState } from "react";
-import { UIQuestion } from "@/app/types/types";
-import { deleteUser, updateUserScore } from "@/app/data/handleUser";
-import { useRouter } from "next/navigation";
+'use client';
+import React, { useState } from 'react';
+import { UIQuestion } from '@/app/types/types';
+import { deleteUser, updateUserScore } from '@/app/data/handleUser';
+import { useRouter } from 'next/navigation';
 
 export type QuizCardProps = {
-    question: UIQuestion;
-    questionCount: number;
-    onNext: (isCorrect: boolean, selected: number) => void;
+  question: UIQuestion;
+  questionCount: number;
+  onNext: (isCorrect: boolean, selected: number) => void;
 };
 
 export default function QuizCard({ question, questionCount, onNext }: QuizCardProps) {
-    const [selected, setSelected] = useState<number>(0);
-    const [answered, setAnswered] = useState(false);
-    const router = useRouter();
+  const [selected, setSelected] = useState<number>(0);
+  const [answered, setAnswered] = useState(false);
+  const router = useRouter();
 
-    const handleAnswer = (option: string) => {
-        setSelected(question.answers.indexOf(option));
-        setAnswered(true);
-    };
+  const handleAnswer = (option: string) => {
+    setSelected(question.answers.indexOf(option));
+    setAnswered(true);
+  };
 
-    const handleNext = async () => {
-        const isCorrect = selected === question.correctAnswer;
-        if (isCorrect) {
-            await updateUserScore()
-        }
-        onNext(isCorrect, selected);
-        setAnswered(false);
-    };
-
-    const handleStartNew = async () => {
-        deleteUser()
-        setAnswered(false)
+  const handleNext = async () => {
+    const isCorrect = selected === question.correctAnswer;
+    if (isCorrect) {
+      await updateUserScore();
     }
+    onNext(isCorrect, selected);
+    setAnswered(false);
+  };
 
-    return (
-        <div
-            className="card w-full max-w-[80vmin] aspect-[4/4] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-3xl p-[clamp(1rem,2vmin,3rem)] flex flex-col justify-between"
-        >
-            {/* Header */}
-            <h2 className="card-title text-center text-[clamp(1.5rem,3vmin,4rem)] font-bold text-white justify-center">
-                {question.text}
-            </h2>
+  const handleStartNew = async () => {
+    deleteUser();
+    setAnswered(false);
+  };
 
-            {/* Antworten */}
-            <div
-                className="flex-1 flex flex-col justify-center gap-[clamp(1rem,3vmin,5rem)] mt-[clamp(1rem,3vmin,5rem)] overflow-y-auto">
-                {question.answers.map((option, index) => (
-                    <button
-                        key={index}
-                        className={`px-[clamp(0.5rem,1vmin,1.5rem)] py-[clamp(0.8rem,2vmin,3rem)] rounded-xl text-[clamp(1rem,2.2vmin,3rem)] font-bold transition-all duration-200 transform hover:scale-105
+  return (
+    <div className="card w-full max-w-[80vmin] aspect-[4/4] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-3xl p-[clamp(1rem,2vmin,3rem)] flex flex-col justify-between">
+      {/* Header */}
+      <h2 className="card-title text-center text-[clamp(1.5rem,3vmin,4rem)] font-bold text-white justify-center">
+        {question.text}
+      </h2>
+
+      {/* Antworten */}
+      <div className="flex-1 flex flex-col justify-center gap-[clamp(1rem,3vmin,5rem)] mt-[clamp(1rem,3vmin,5rem)]">
+        {question.answers.map((option, index) => (
+          <button
+            key={index}
+            className={`px-[clamp(0.5rem,1vmin,1.5rem)] py-[clamp(0.8rem,2vmin,3rem)] rounded-xl text-[clamp(1rem,2.2vmin,3rem)] font-bold transition-all duration-200 transform hover:scale-105
             ${
-                            answered && selected === index
-                                ? "bg-cyan-500 text-white border-cyan-700"
-                                : "bg-gray-800 text-white border-gray-300 hover:bg-gray-100"
-                        }`}
-                        onClick={() => handleAnswer(option)}
-                    >
-                        {option}
-                    </button>
-                ))}
-            </div>
+              answered && selected === index
+                ? 'bg-cyan-500 text-white border-cyan-700'
+                : 'bg-gray-800 text-white border-gray-300 hover:bg-gray-100'
+            }`}
+            onClick={() => handleAnswer(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
 
-            {/* Footer */}
-            <div className="flex flex-col gap-[clamp(0.5rem,1.5vmin,2rem)] mt-[clamp(1rem,2vmin,2rem)]">
-                <div className="flex gap-[clamp(0.5rem,1.5vmin,2rem)]">
-                    <button
-                        className="btn flex-1 py-[clamp(0.5rem,2vmin,3rem)] text-[clamp(0.9rem,2.2vmin,3rem)] bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 text-white rounded-xl shadow-lg transition-all"
-                        onClick={() => {
-                            handleStartNew();
-                            router.push(`/`);
-                        }}
-                    >
-                        Start new
-                    </button>
-                    <button
-                        className="btn flex-1 py-[clamp(0.5rem,2vmin,3rem)] text-[clamp(0.9rem,2.2vmin,3rem)] bg-gradient-to-r from-green-500 to-teal-500 hover:from-teal-500 hover:to-green-500 text-white rounded-xl shadow-lg transition-all"
-                        onClick={handleNext}
-                        disabled={!answered}
-                    >
-                        Next
-                    </button>
-                </div>
-
-                {/* Progress */}
-                <progress
-                    className="progress h-[clamp(0.5rem,1.5vmin,1.5rem)] rounded-full overflow-hidden"
-                    value={(100 * questionCount) / 3}
-                    max={100}
-                />
-
-                <p className="text-[clamp(0.9rem,2vmin,3rem)] text-center text-white opacity-80">
-                    Question {questionCount} of 3
-                </p>
-            </div>
+      {/* Footer */}
+      <div className="flex flex-col gap-[clamp(0.5rem,1.5vmin,2rem)] mt-[clamp(1rem,2vmin,2rem)]">
+        <div className="flex gap-[clamp(0.5rem,1.5vmin,2rem)]">
+          <button
+            className="btn flex-1 py-[clamp(0.5rem,2vmin,3rem)] text-[clamp(0.9rem,2.2vmin,3rem)] bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 text-white rounded-xl shadow-lg transition-all"
+            onClick={() => {
+              handleStartNew();
+              router.push(`/`);
+            }}
+          >
+            Start new
+          </button>
+          <button
+            className="btn flex-1 py-[clamp(0.5rem,2vmin,3rem)] text-[clamp(0.9rem,2.2vmin,3rem)] bg-gradient-to-r from-green-500 to-teal-500 hover:from-teal-500 hover:to-green-500 text-white rounded-xl shadow-lg transition-all"
+            onClick={handleNext}
+            disabled={!answered}
+          >
+            Next
+          </button>
         </div>
-    );
+
+        {/* Progress */}
+        <progress
+          className="progress h-[clamp(0.5rem,1.5vmin,1.5rem)] rounded-full overflow-hidden"
+          value={(100 * questionCount) / 3}
+          max={100}
+        />
+
+        <p className="text-[clamp(0.9rem,2vmin,3rem)] text-center text-white opacity-80">
+          Question {questionCount} of 3
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/app/components/QuizFeedback.tsx
+++ src/app/components/QuizFeedback.tsx
@@ -1,70 +1,68 @@
-'use client'
+'use client';
 
-import { UIQuestion } from "@/app/types/types";
+import { UIQuestion } from '@/app/types/types';
 
 type FeedbackProps = {
-    question: UIQuestion;
-    selectedIndex: number;
-    onNext: () => void;
+  question: UIQuestion;
+  selectedIndex: number;
+  onNext: () => void;
 };
 
 export default function QuizFeedback({ question, selectedIndex, onNext }: FeedbackProps) {
-    const isCorrect = selectedIndex === question.correctAnswer;
-    const handleNext = async (): Promise<void> => {
-        onNext()
-    }
-        return (
-            <div className="card w-full max-w-[80vmin] aspect-[4/4] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-3xl p-[clamp(1rem,2vmin,3rem)] flex flex-col justify-between overflow-hidden">
+  const isCorrect = selectedIndex === question.correctAnswer;
+  const handleNext = async (): Promise<void> => {
+    onNext();
+  };
+  return (
+    <div className="card w-full max-w-[80vmin] aspect-[4/4] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-3xl p-[clamp(1rem,2vmin,3rem)] flex flex-col justify-between overflow-hidden">
+      {/* Header: Feedback */}
+      <div
+        className={`text-center font-bold py-[clamp(0.8rem,4vmin,5rem)] text-[clamp(1.5rem,3vmin,4rem)] -mx-[clamp(1rem,2vmin,3rem)] -mt-[clamp(1rem,2vmin,3rem)] mb-[clamp(1rem,2vmin,3rem)]"
+                    ${isCorrect ? 'bg-green-100 text-green-700' : 'bg-red-200 text-red-700'} shadow-md`}
+      >
+        {isCorrect ? 'Right Answer' : 'Wrong Answer'}
+      </div>
 
-                {/* Header: Feedback */}
-              <div
-                className={`text-center font-bold py-[clamp(0.8rem,4vmin,5rem)] text-[clamp(1.5rem,3vmin,4rem)] -mx-[clamp(1rem,2vmin,3rem)] -mt-[clamp(1rem,2vmin,3rem)] mb-[clamp(1rem,2vmin,3rem)]"
-                    ${isCorrect ? "bg-green-100 text-green-700" : "bg-red-200 text-red-700"} shadow-md`}
-              >
-                {isCorrect ? "Right Answer" : "Wrong Answer"}
-              </div>
+      {/* Body: Question + Answers + Feedback + Leaderboard */}
+      <div className="flex flex-1 gap-[clamp(1rem,2vmin,2rem)] mt-[clamp(1rem,2vmin,2rem)]">
+        {/* Question + Answers */}
+        <div className="flex-1 flex flex-col gap-[clamp(0.5rem,1vmin,1.5rem)]">
+          {/* Question */}
+          <div className="text-center text-[clamp(1.5rem,3vmin,4rem)] font-semibold text-white p-[clamp(0.5rem,1vmin,1rem)]">
+            {question.text}
+          </div>
 
-                {/* Body: Question + Answers + Feedback + Leaderboard */}
-                <div className="flex flex-1 gap-[clamp(1rem,2vmin,2rem)] mt-[clamp(1rem,2vmin,2rem)] overflow-y-auto">
-
-                    {/* Question + Answers */}
-                    <div className="flex-1 flex flex-col gap-[clamp(0.5rem,1vmin,1.5rem)]">
-                        {/* Question */}
-                        <div className="text-center text-[clamp(1.5rem,3vmin,4rem)] font-semibold text-white p-[clamp(0.5rem,1vmin,1rem)]">
-                            {question.text}
-                        </div>
-
-                        {/* Answers */}
-                        <div className="flex flex-col gap-[clamp(0.5rem,1vmin,1rem)]">
-                            {question.answers.map((answer, index) => {
-                                const isCorrectAnswer = index === question.correctAnswer;
-                                const isSelected = index === selectedIndex;
-                                return (
-                                    <div
-                                        key={index}
-                                        className={`px-[clamp(0.5rem,1vmin,1rem)] py-[clamp(0.5rem,1vmin,1.5rem)] rounded-xl text-[clamp(1.2rem,2vmin,3rem)] 
+          {/* Answers */}
+          <div className="flex flex-col gap-[clamp(0.5rem,1vmin,1rem)]">
+            {question.answers.map((answer, index) => {
+              const isCorrectAnswer = index === question.correctAnswer;
+              const isSelected = index === selectedIndex;
+              return (
+                <div
+                  key={index}
+                  className={`px-[clamp(0.5rem,1vmin,1rem)] py-[clamp(0.5rem,1vmin,1.5rem)] rounded-xl text-[clamp(1.2rem,2vmin,3rem)] 
                                                   font-bold transition-all duration-200 transform hover:scale-105
-                                                  ${isCorrectAnswer ? "border-green-400 bg-green-600 text-white" : ""}
-                                                  ${isSelected && !isCorrectAnswer ? "border-red-400 bg-red-500 text-white" : ""}
-                                                  ${!isCorrectAnswer && !isSelected ? "border-slate-300 bg-slate-600 text-white" : ""}`}
-                                    >
-                                        {answer}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
+                                                  ${isCorrectAnswer ? 'border-green-400 bg-green-600 text-white' : ''}
+                                                  ${isSelected && !isCorrectAnswer ? 'border-red-400 bg-red-500 text-white' : ''}
+                                                  ${!isCorrectAnswer && !isSelected ? 'border-slate-300 bg-slate-600 text-white' : ''}`}
+                >
+                  {answer}
                 </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
 
-                {/* Footer */}
-                <div className="flex flex-col gap-[clamp(0.5rem,1vmin,1.5rem)] mt-[clamp(1rem,2vmin,2rem)]">
-                    <button
-                        className="btn w-full py-[clamp(0.5rem,2.5vmin,3rem)] text-[clamp(1rem,2vmin,3rem)] bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 text-white rounded-xl shadow-lg transition-all"
-                        onClick={handleNext}
-                    >
-                        Next Question
-                    </button>
-                </div>
-            </div>
-        );
-    }
+      {/* Footer */}
+      <div className="flex flex-col gap-[clamp(0.5rem,1vmin,1.5rem)] mt-[clamp(1rem,2vmin,2rem)]">
+        <button
+          className="btn w-full py-[clamp(0.5rem,2.5vmin,3rem)] text-[clamp(1rem,2vmin,3rem)] bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 text-white rounded-xl shadow-lg transition-all"
+          onClick={handleNext}
+        >
+          Next Question
+        </button>
+      </div>
+    </div>
+  );
+}
