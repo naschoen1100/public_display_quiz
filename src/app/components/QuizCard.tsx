dfe26c57d71197a176cbbf57ba5f932efe4301db
Sherif Lezi <73194598+Goldinggamer@users.noreply.github.com>
2026-01-19 09:55:08 +0100

    hide progress bar

--- src/app/components/QuizCard.tsx
+++ src/app/components/QuizCard.tsx
@@ -37,7 +37,7 @@ export default function QuizCard({ question, questionCount, onNext }: QuizCardPr
   return (
     <div className="card w-full max-w-[80vmin] aspect-[4/4] bg-gradient-to-br from-cyan-700 to-cyan-900 shadow-2xl rounded-3xl p-[clamp(1rem,2vmin,3rem)] flex flex-col justify-between">
       {/* Header */}
-      <h2 className="card-title text-center text-[clamp(1.5rem,3vmin,5rem)] font-bold text-white justify-center">
+      <h2 className="card-title text-center text-[clamp(1.5rem,3vmin,4rem)] font-bold text-white justify-center">
         {question.text}
       </h2>
 
@@ -46,11 +46,11 @@ export default function QuizCard({ question, questionCount, onNext }: QuizCardPr
         {question.answers.map((option, index) => (
           <button
             key={index}
-            className={`px-[clamp(0.5rem,1vmin,1.5rem)] py-[clamp(0.8rem,2vmin,3rem)] rounded-xl text-[clamp(1rem,2.5vmin,4rem)] font-bold transition-all duration-200 transform hover:scale-105
+            className={`px-[clamp(0.5rem,1vmin,1.5rem)] py-[clamp(0.8rem,2vmin,3rem)] rounded-xl text-[clamp(1rem,2.2vmin,3rem)] font-bold transition-all duration-200 transform hover:scale-105
             ${
               answered && selected === index
                 ? 'bg-cyan-500 text-white border-cyan-700'
-                : 'bg-gray-800 text-white border-gray-300 hover:bg-gray-400'
+                : 'bg-gray-800 text-white border-gray-300 hover:bg-gray-100'
             }`}
             onClick={() => handleAnswer(option)}
           >
@@ -81,11 +81,11 @@ export default function QuizCard({ question, questionCount, onNext }: QuizCardPr
         </div>
 
         {/* Progress */}
-        <progress
+        {/* <progress
           className="progress h-[clamp(0.5rem,1.5vmin,1.5rem)] rounded-full overflow-hidden"
           value={(100 * questionCount) / 3}
           max={100}
-        />
+        />*/}
 
         <p className="text-[clamp(0.9rem,2vmin,3rem)] text-center text-white opacity-80">
           Question {questionCount} of 3
