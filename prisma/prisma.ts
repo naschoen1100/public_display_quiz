8a99b9c0f9207c4a5eedd0f85a3b1e5fe7f4b9d9
Sherif Lezi <73194598+Goldinggamer@users.noreply.github.com>
2026-01-19 12:50:36 +0100

    changed something for sqlite

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const adapter = new PrismaBetterSqlite3({
-  url: "file:./prisma/dev.db",
+  url: "file:prisma/dev.db",
 });
 const prisma = new PrismaClient({ adapter });
 
