71e05328949e47521154f4405561f3eaaecb7280
naschoen1100 <natalie.s.schoenfeld@posteo.de>
2026-01-19 13:57:12 +0100

    added mac adjustments

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from '@/app/generated/prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const adapter = new PrismaBetterSqlite3({
-  url: "file:prisma/dev.db",
+  url: 'file::/prisma/dev.db',
+  //url: "file:prisma/dev.db",
 });
 const prisma = new PrismaClient({ adapter });
 
